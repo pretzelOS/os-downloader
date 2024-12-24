8443f31c31de0a18a6f0c6faaec205426035486c
Camden Heather <cmdenpcs@icloud.com>
2024-12-24 11:09:25 -0500

    ContentView.swift
    The content of the OS downloader.

---
+++ ContentView.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("PretzelOS Downloader").font(.largeTitle).fontWeight(.bold).frame(maxWidth: .infinity).padding()
+             .frame(alignment: .top)
+            Button("Download") {
+              
+            }
+            Text("-------------------")
+            
+                
+        }
+    }
+}
